25bcf035cc4530bd00013aa8bfafcbd12ff0d453
EnzoDattignie <enzo.dattignie@gmail.com>
2025-09-28 12:08:37 +0200

    26-09

--- main.c
+++ main.c
@@ -6,14 +6,19 @@
 double sigma = 1;
 double epsilon = 1;
 #define M 1;
-static double dt = 0.1;
-static double L = 4;
-const int n = 5;
+static double dt = 0.01;
+static double L = 3;
+const int n = 3;
 static int N = n*n;
+// static int N = 2;
 static double T = 1;
-static double t_max = 1;
+static double t_max = 100;
 
 int main () {
+    FILE *fichier;
+    fichier = fopen("Mesure.txt","w");
+    fprintf(fichier,"Temps;Energie\n");
+    fclose(fichier);
     srand(time(0));
     struct Part {
         double x;
@@ -22,6 +27,7 @@ int main () {
         double vy;
         double ax;
         double ay;
+        double u;
         double m;
     };
 
@@ -32,6 +38,7 @@ int main () {
         P->ay = 0;
         P->vx = 0;
         P->vy = 0;
+        P->u = 0;
         P->m = M;
         return 0;
     }
@@ -40,6 +47,7 @@ int main () {
         printf("x = %f : y = %f\n",P->x,P->y);
         printf("vx = %f : vy = %f\n",P->vx,P->vy);
         printf("ax = %f : ay = %f\n\n",P->ax,P->ay);
+        printf("u = %f\n", P->u);
         return 0;
     }
 
@@ -49,7 +57,7 @@ int main () {
         return rd;
     }
 
-    double modulo(double x){
+    double modulo(double x,double y){
         double res = x;
         if (res>L/2) {
             long div = (long) (res+L/2)/L;
@@ -66,15 +74,48 @@ int main () {
     int update(struct Part *P) {
         P->vx = P->vx + P->ax * dt;
         P->vy = P->vy + P->ay * dt;
-        P->x = modulo(P->x + P->vx * dt);
-        P->y = modulo(P->y + P->vy * dt);
+        P->x = P->x + P->vx * dt;
+        P->y = P->y + P->vy * dt;
         P->ax = 0;
         P->ay = 0;
+        P->u = 0;
         return 0;
     }
 
+    int fct_u(struct Part *P1, struct Part *P2) {
+        double dx = P2->x-P1->x;
+        double dy = P2->y-P1->y;
+        double r2 = sigma*sigma/(dx*dx + dy*dy);
+        double r6 = r2*r2*r2;
+        double r12 = r6*r6;
+        double val_u = 4*(r12-r6);
+        P1->u = P1->u + val_u;
+        P2->u = P2->u + val_u;
+        return 0;
+    }
+
+    int update_u(struct Part Liste[]) {
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                fct_u(&Liste[i],&Liste[j]); 
+            }
+        }
+        return 0;
+    }
+
+    int somme_E(struct Part Liste[], double *E_cin,double *E_pot) {
+        *E_cin = 0;
+        *E_pot = 0;
+        for (int i = 0; i < N; i++) {
+            *E_cin = *E_cin+Liste[i].m*(Liste[i].vx*Liste[i].vx + Liste[i].vy*Liste[i].vy);
+            *E_pot = *E_pot+Liste[i].u;
+        }
+    }
+
+
+
     int Force(struct Part *P1, struct Part *P2) {
-        double dx = P2->x-P1->x;;
+        double dx = P2->x-P1->x;
         double dy = P2->y-P1->y;
         double r = sqrt(dx*dx + dy*dy);
     
@@ -134,29 +175,48 @@ int main () {
         return 0;
     }
 
+    double modulo2(double x, double y) {
+        int div = x/y;
+        return x-div*y;
+    }
+
+
+    double E_cin = 0;
+    double E_pot = 0;
     
 
     struct Part Liste[N];
     config_crist(Liste);
+    // constructeur(&Liste[0],0,0);
+    // constructeur(&Liste[0],1,1);
 
     // for (int i = 0; i < N;i++) {
     //     constructeur(&Liste[i],rng(),rng());
     // }
 
-    // for (double t = 0; t < t_max;t=t+dt) {
-    //     Force_liste(Liste);
-    //     for (int i = 0; i < N;i++) {
-    //         afficher(&Liste[i]);
-    //     }
-    //     update_Liste(Liste);
-    //     printf("Temps : %f\n",t);
-    // }
+    update_u(Liste);
+    somme_E(Liste,&E_cin,&E_pot);
+    printf("Energie initiale %f\n",E_cin+E_pot);
+    fichier = fopen("Mesure.txt","a");
+    for (double t = 0; t < t_max;t=t+dt) {
+        Force_liste(Liste);
+        // for (int i = 0; i < N;i++) {
+        //     afficher(&Liste[i]);
+        // }
+        update_Liste(Liste);
+        update_u(Liste);
+        somme_E(Liste,&E_cin,&E_pot);
+        fprintf(fichier,"%f;%f\n",t,E_cin+E_pot);
 
-
-
-    for (int i = 0; i < N;i++) {
-        afficher(&Liste[i]);
+        if (fabs(modulo2(t,1)-0.1)<0.1*dt) {
+        printf("Temps : %f; Energie : %f\n",t,E_cin+E_pot);
+        }
+        
     }
+    fclose(fichier);
+    // for (int i = 0; i < N;i++) {
+    //     afficher(&Liste[i]);
+    // }
     
 
     // constructeur(&P2,1.5,0);
