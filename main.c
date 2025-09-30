0c3b184ec0325beb451e65b9a55dc9f5c02aa194
EnzoDattignie <enzo.dattignie@gmail.com>
2025-09-30 16:22:17 +0200

    Commentage partiel

--- main.c
+++ main.c
@@ -3,31 +3,37 @@
 #include <math.h>
 #include <time.h>
 
+// ==== Initialisation des constantes ==== //
 
-static char nom_fichier[] = "./temp.txt";
-
-const double t_star = 1;
-const double n_pas = 1e6  ;
-static double dt = t_star/n_pas;
-
-// static double dt = 0.001;
-
+#define N (15) //Nombre total de particule
 double sigma = 1;
 double epsilon = 1;
-static double M = 1;
-static double L = 3;
-const int n = 3;
-static int N = 15;
-static double dl = 0.70 ;
-static double T = 1;
+
+static char nom_fichier[] = "./temp.txt"; //Nom du fichier d'enregistrement
+
+const double t_star = 1; 
+const double n_pas = 1e6  ;
+static double dt = t_star/n_pas;
 static double t_max = 10;
 
+static double M = 1;
+static int n = sqrt(N);
+
+static double L = 3;
+
+static double dl = 0.70 ;
+// static double dl = (n+1)/L //Ne marche que si N est un carré
+
+
 int main () {
+    // ==== Initialisation ==== //
     FILE *fichier;
     fichier = fopen(nom_fichier,"w");
     fprintf(fichier,"dt = %f\nTemps;Energie\n",dt);
     fclose(fichier);
     srand(time(0));
+
+    // ==== Fonctions ==== //
     struct Part {
         double x;
         double y;
@@ -77,17 +83,6 @@ int main () {
             // printf("%f : %li : %f\n",res,div,x);
         }
         return res;
-    }
-
-    int Euler(struct Part *P) {
-        P->vx = P->vx + P->ax * dt;
-        P->vy = P->vy + P->ay * dt;
-        P->x = P->x + P->vx * dt;
-        P->y = P->y + P->vy * dt;
-        P->ax = 0;
-        P->ay = 0;
-        P->u = 0;
-        return 0;
     }
 
     int fct_u(struct Part *P1, struct Part *P2) {
@@ -156,14 +151,22 @@ int main () {
         return 0;
     }
 
-    int Euler_Liste(struct Part Liste[]) {
+    int Euler_step(struct Part Liste[],double *E_cin, double *E_pot) {
+        update_u(Liste);
+        somme_E(Liste,E_cin,E_pot);
         for (int i = 0;i<N;i++) {
-            Euler(&Liste[i]);
+            Liste[i].vx = Liste[i].vx + Liste[i].ax * dt;
+            Liste[i].vy = Liste[i].vy + Liste[i].ay * dt;
+            Liste[i].x = Liste[i].x + Liste[i].vx * dt;
+            Liste[i].y = Liste[i].y + Liste[i].vy * dt;
+            Liste[i].ax = 0;
+            Liste[i].ay = 0;
         }
+        Force_liste(Liste);
         return 0;
     }
 
-    int Verlet_Liste(struct Part Liste[],double *E_cin, double *E_pot) {
+    int Verlet_step(struct Part Liste[],double *E_cin, double *E_pot) {
         double old_ax[N];
         double old_ay[N];
         update_u(Liste);
@@ -184,35 +187,19 @@ int main () {
         return 0;
     }
 
-    int config_crist(struct Part Liste[]) {
-        double l = L/(n);
-        int compteur = 0;
-        double posx = (l-L)/2;
-        double posy = (l-L)/2;
-        for (int i = 0;i<N;i++) {
-            compteur ++;
-            constructeur(&Liste[i],posx,posy);
-            posx = posx + dl;
-            if (compteur == n) {
-                posy = posy + dl;
-                posx = (dl-L)/2;;
-                compteur = 0;
-            }
-        }
-        return 0;
-    }
 
-    int config_crist2(struct Part Liste[]) {
+    int config_crist(struct Part Liste[]) {
         int compteur = 0;
-        double posx = 0;
-        double posy = 0;
+        double origin = (-L+dl)/2;
+        double posx = origin;
+        double posy = origin;
         for (int i = 0;i<N;i++) {
             compteur ++;
             constructeur(&Liste[i],posx,posy);
             posx = posx + dl;
             if (compteur == n) {
                 posy = posy + dl;
-                posx = 0;
+                posx = origin;
                 compteur = 0;
             }
         }
@@ -225,70 +212,45 @@ int main () {
     }
 
 
+    // ==== Initialisation du système ==== //
+
     double E_cin = 0;
     double E_pot = 0;
     
 
     struct Part Liste[N];
-    config_crist2(Liste);
-    // constructeur(&Liste[0],0,0);
-    // constructeur(&Liste[0],1,1);
+    config_crist(Liste);
 
-    // for (int i = 0; i < N;i++) {
+    // for (int i = 0; i < N;i++) { // config random
     //     constructeur(&Liste[i],rng(),rng());
     // }
-
+    Force_liste(Liste);
     update_u(Liste);
     somme_E(Liste,&E_cin,&E_pot);
-
-    // double dE = (E-(E_cin+E_pot))/N;
-    // double dv = sqrt(dE/M);
-    // for (int i = 0; i < N;i++) {
-    //     double theta = rng()*2*M_PI;
-    //     Liste[i].vx = Liste[i].vx+dv*cos(theta);
-    //     Liste[i].vy = Liste[i].vx+dv*sin(theta);
-    // }
-
-
     printf("Energie initiale %f\nE_cin = %f : E_pot = %f\n",E_cin+E_pot,E_cin, E_pot);
+
     fichier = fopen(nom_fichier,"a");
     fprintf(fichier,"%f;%f\n",0.0,E_cin+E_pot);
+    
+    // ==== Boucle d'itération principale ==== //
     double compteur = 0;
-    Force_liste(Liste);
+    double compteur2 = 0;
     for (double t = 0; t < t_max;t=t+dt) {
-        // update_u(Liste);
-        // somme_E(Liste,&E_cin,&E_pot);
-        // Euler_Liste(Liste);
-        // Force_liste(Liste);
-        Verlet_Liste(Liste,&E_cin,&E_pot);
-        if (compteur >= t_max/20-0.5*dt) {
-            fprintf(fichier,"%f;%f\n",t,E_cin+E_pot);
+        // Euler_step(Liste,&E_cin,&E_pot);
+        Verlet_step(Liste,&E_cin,&E_pot);
+        if (compteur >= t_max/20-0.5*dt) { //Permet d'afficher 20 valeurs
             printf("Temps : %f; Energie : %f\n",t,E_cin+E_pot);
             compteur = 0;
         }
+        if (compteur2 >= t_max/1000-0.5*dt) { //Permet d'enregistrer 1000 valeurs
+            fprintf(fichier,"%f;%f\n",t,E_cin+E_pot);
+            compteur2 = 0;
+        }
         compteur = compteur + dt;
-        
+        compteur2 = compteur2 + dt;
     }
+
+    // ==== Fin du programme ==== //
     fclose(fichier);
-    // for (int i = 0; i < N;i++) {
-    //     afficher(&Liste[i]);
-    // }
-    
-
-    // constructeur(&P2,1.5,0);
-    // double t;
-    // static double t_max = 10;
-    // for (t = 0; t<T; t = t + 0.1) {
-    //     Force(&P1,&P2);
-    //     afficher(&P1);
-    //     afficher(&P2);
-    //     update(&P1);
-    //     update(&P2);
-    //     printf("%f : %f\n",rng(),rng());
-    // }
-    // afficher(&P1);
-    // afficher(&P2);
-    // printf("%f : %f : %f",modulo(0.4),modulo(234),modulo(-654.3));
-
     return 0;
 }
