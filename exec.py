c401e7c283933cb61fc834964cda68d981a08ae3
EnzoDattignie <enzo.dattignie@gmail.com>
2025-10-14 19:02:56 +0200

    eh

--- exec.py
+++ exec.py
@@ -1,2 +1,33 @@
 import subprocess
-subprocess.run(["./test.out","1e6","3"])
+import matplotlib.pyplot as plt
+import os
+
+
+times = ["1e2","3e2"]
+modes = ["0","1"]
+t_star = "1"
+
+samples = int(1e5)
+for mode in modes :
+    dic = {}
+    for time in times :
+        # os.startfile("./test.out "+time+" "+mode+" "+t_star)
+        subprocess.run(["./test.out",time,mode,t_star])
+        with open ("./temp.txt") as f:
+            compteur = -2
+            dt = 0
+            E = 0
+            t = 0
+            for Line in f :
+                if (compteur==-2):
+                    dt = Line.split("=")[1].strip()
+                if compteur>=0 :
+                    t[compteur] = Line.split(";")[0].strip()
+                    E[compteur] = Line.split(";")[1].strip()
+                compteur += 1
+            E = E[:compteur]
+            t = t[:compteur]
+            plt.plot(t,E)
+            plt.show()
+            dic[dt]=E
+     
